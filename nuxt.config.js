ab075b58da0434c84a3e1e30798c8ccb08747ba2
Ricki Moore <mavrik.development@gmail.com>
2018-06-01 13:50:36 +0200

    added config files

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,10 @@ const config = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Custom app in under 5 minutes' }
+      { hid: 'description', name: 'description', content: 'Custom app in under 5 minutes' },
+      {property: 'og:image', content: 'http://www.mavrik.io/images/mavrik.png'},
+      {property: 'og:title', content: 'Whipped Lashes by Michelle Mendoza'},
+      {property: 'og:description', content: 'This app was created by Mavrik.io'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
